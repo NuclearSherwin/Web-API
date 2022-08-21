160774dca58d328b5f985ef07aea611b378f2215
paika2060 <paika2060@gmail.com>
2022-08-21 12:17:29 +0700

    create a get api method

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+
+        public static List<Student> students = new List<Student>
+        {
+            new Student
+            {
+                Id = 1,
+                Name = "Paika",
+                FirstName = "Phong",
+                LastName = "Sherwin",
+                Place = "New York City"
+            }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> GetAll()
+        {
+            return Ok(students);
+        }
+    }
+}
