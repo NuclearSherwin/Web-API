41bf920ead79efd3834cf08e776f81a0d84f42fd
paika2060 <paika2060@gmail.com>
2022-08-21 13:07:23 +0700

    add connection string to conenct to sql server

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -15,7 +15,17 @@ namespace WebApi.Controllers
                 Name = "Paika",
                 FirstName = "Phong",
                 LastName = "Sherwin",
-                Place = "New York City"
+                Place = "New York City",
+                Special = "Intelligence"
+            },
+            new Student
+            {
+                Id = 2,
+                Name = "Spider Man",
+                FirstName = "Peter",
+                LastName = "Parker",
+                Place = "New York City",
+                Special = "Spider web"
             }
         };
 
@@ -23,6 +33,45 @@ namespace WebApi.Controllers
         public async Task<ActionResult<List<Student>>> GetAll()
         {
             return Ok(students);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> Get(int id)
+        {
+            var student = students.Find(s => s.Id == id);
+            if (student == null) return BadRequest("Student not found");
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Student>>> AddStudent(Student student)
+        {
+            students.Add(student);
+            return Ok(students);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<Student>>> UpdateStudent(Student studentObj)
+        {
+            var student = students.Find(s => s.Id == studentObj.Id);
+            if (student == null) return BadRequest("Student not found");
+            student.Name = studentObj.Name;
+            student.FirstName = studentObj.FirstName;
+            student.LastName = studentObj.LastName;
+            student.Place = studentObj.Place;
+            student.Special = studentObj.Special;
+
+            return Ok(students);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Student>>> Delete(int id)
+        {
+            var student = students.Find(s => s.Id == id);
+            if (student == null) return BadRequest("Student not found");
+            students.Remove(student);
+
+            return Ok(student);
         }
     }
 }
